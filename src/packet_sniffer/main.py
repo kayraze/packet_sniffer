ffc95d4cec6bf206d3ba62a8daa4bee015119fc4
kayraze <jadenspitzky@gmail.com>
2025-08-10 23:55:26 +0800

    add argument parsing

--- src/packet_sniffer/main.py
+++ src/packet_sniffer/main.py
@@ -1,13 +1,48 @@
 from .sniffing.capture import PacketSniffer
+import argparse
 
 def main():
     print("[main]")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'filter', 
+        nargs="?",
+        default="",
+        help="BPF filter expression for capturing (e.g., 'tcp', 'udp', 'port 80')"
+    )
+    parser.add_argument(
+        'interface',
+        nargs="?",
+        default=None,
+        help='The interface to listen on (positional)'
+    )
+    
+
+    parser.add_argument(
+        "-i", "--interface",
+        dest="interface_flag",
+        help="The interface to listen on (flag version)"
+    )
+
+    parser.add_argument(
+        '-pc', '--packet-count', 
+        type=int,
+        default=0,
+        help='How many packets to capture before stopping'
+        )
+    args = parser.parse_args()
+
+    interface = args.interface_flag or args.interface
+    
     sniffer = PacketSniffer(
-        interface="wlp4s0",
-        packet_count=10,
-        filter_exp="tcp"
+        interface=interface,
+        packet_count=args.packet_count,
+        filter_exp=args.filter
     )
     sniffer.start()
 
 if __name__ == "__main__":
+
+
     main()
